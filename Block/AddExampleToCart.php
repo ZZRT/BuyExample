a2dec90ebd232ad9d8cb42dd0d27cf39ccd9b6d1
Eugene Malofienko <eugene.malofienko@wdg.com.ua>
2018-12-06 17:13:19 +0200

    Created block for adding product example to cart, added reference it in layout after product.info.addtocart

---
+++ Block/AddExampleToCart.php
@@ -0,0 +1,262 @@
+<?php
+namespace Wise\BuyExample\Block;
+
+use Magento\Catalog\Block\Product\AbstractProduct;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Category;
+use Magento\Framework\Exception\NoSuchEntityException;
+class AddExampleToCart extends AbstractProduct implements \Magento\Framework\DataObject\IdentityInterface
+{
+    /**
+     * Magento string lib
+     *
+     * @var \Magento\Framework\Stdlib\StringUtils
+     */
+    protected $string;
+
+    /**
+     * @var \Magento\Framework\Json\EncoderInterface
+     */
+    protected $_jsonEncoder;
+
+    /**
+     * @var \Magento\Framework\Pricing\PriceCurrencyInterface
+     * @deprecated 102.0.0
+     */
+    protected $priceCurrency;
+
+    /**
+     * @var \Magento\Framework\Url\EncoderInterface
+     */
+    protected $urlEncoder;
+
+    /**
+     * @var \Magento\Catalog\Helper\Product
+     */
+    protected $_productHelper;
+
+    /**
+     * @var \Magento\Catalog\Model\ProductTypes\ConfigInterface
+     */
+    protected $productTypeConfig;
+
+    /**
+     * @var \Magento\Framework\Locale\FormatInterface
+     */
+    protected $_localeFormat;
+
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $customerSession;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param \Magento\Framework\Url\EncoderInterface $urlEncoder
+     * @param \Magento\Framework\Json\EncoderInterface $jsonEncoder
+     * @param \Magento\Framework\Stdlib\StringUtils $string
+     * @param \Magento\Catalog\Helper\Product $productHelper
+     * @param \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig
+     * @param \Magento\Framework\Locale\FormatInterface $localeFormat
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface|\Magento\Framework\Pricing\PriceCurrencyInterface $productRepository
+     * @param \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency
+     * @param array $data
+     * @codingStandardsIgnoreStart
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Framework\Url\EncoderInterface $urlEncoder,
+        \Magento\Framework\Json\EncoderInterface $jsonEncoder,
+        \Magento\Framework\Stdlib\StringUtils $string,
+        \Magento\Catalog\Helper\Product $productHelper,
+        \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig,
+        \Magento\Framework\Locale\FormatInterface $localeFormat,
+        \Magento\Customer\Model\Session $customerSession,
+        ProductRepositoryInterface $productRepository,
+        \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency,
+        array $data = []
+    ) {
+        $this->_productHelper = $productHelper;
+        $this->urlEncoder = $urlEncoder;
+        $this->_jsonEncoder = $jsonEncoder;
+        $this->productTypeConfig = $productTypeConfig;
+        $this->string = $string;
+        $this->_localeFormat = $localeFormat;
+        $this->customerSession = $customerSession;
+        $this->productRepository = $productRepository;
+        $this->priceCurrency = $priceCurrency;
+        parent::__construct(
+            $context,
+            $data
+        );
+    }
+
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Return wishlist widget options
+     *
+     * @return array
+     * @deprecated 101.0.1
+     */
+    public function getWishlistOptions()
+    {
+        return ['productType' => $this->getProduct()->getTypeId()];
+    }
+
+    /**
+     * Retrieve current product model
+     *
+     * @return \Magento\Catalog\Model\Product
+     * @throws NoSuchEntityException
+     */
+    public function getProduct()
+    {
+        try{
+            if (!$this->_coreRegistry->registry('product') && $this->getProductId()) {
+                $product = $this->productRepository->getById($this->getProductId());
+                $this->_coreRegistry->register('product', $product);
+            }
+            return $this->_coreRegistry->registry('product');
+        } catch (NoSuchEntityException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * Check if product can be emailed to friend
+     *
+     * @return bool
+     */
+    public function canEmailToFriend()
+    {
+        return false;
+    }
+
+    /**
+     * Get JSON encoded configuration array which can be used for JS dynamic
+     * price calculation depending on product options
+     *
+     * @return string
+     */
+    public function getJsonConfig()
+    {
+        /* @var $product \Magento\Catalog\Model\Product */
+        $product = $this->getProduct();
+
+        if (!$this->hasOptions()) {
+            $config = [
+                'productId' => $product->getId(),
+                'priceFormat' => $this->_localeFormat->getPriceFormat()
+            ];
+            return $this->_jsonEncoder->encode($config);
+        }
+
+        $tierPrices = [];
+        $tierPricesList = $product->getPriceInfo()->getPrice('tier_price')->getTierPriceList();
+        foreach ($tierPricesList as $tierPrice) {
+            $tierPrices[] = $tierPrice['price']->getValue();
+        }
+        $config = [
+            'productId'   => $product->getId(),
+            'priceFormat' => $this->_localeFormat->getPriceFormat(),
+            'prices'      => [
+                'oldPrice'   => [
+                    'amount'      => $product->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue(),
+                    'adjustments' => []
+                ],
+                'basePrice'  => [
+                    'amount'      => $product->getPriceInfo()->getPrice('final_price')->getAmount()->getBaseAmount(),
+                    'adjustments' => []
+                ],
+                'finalPrice' => [
+                    'amount'      => $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue(),
+                    'adjustments' => []
+                ]
+            ],
+            'idSuffix'    => '_clone',
+            'tierPrices'  => $tierPrices
+        ];
+
+        $responseObject = new \Magento\Framework\DataObject();
+        $this->_eventManager->dispatch('catalog_product_view_config', ['response_object' => $responseObject]);
+        if (is_array($responseObject->getAdditionalOptions())) {
+            foreach ($responseObject->getAdditionalOptions() as $option => $value) {
+                $config[$option] = $value;
+            }
+        }
+
+        return $this->_jsonEncoder->encode($config);
+    }
+
+    /**
+     * Return true if product has options
+     *
+     * @return bool
+     */
+    public function hasOptions()
+    {
+        if ($this->getProduct()->getTypeInstance()->hasOptions($this->getProduct())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get default qty - either as preconfigured, or as 1.
+     * Also restricts it by minimal qty.
+     *
+     * @param null|\Magento\Catalog\Model\Product $product
+     * @return int|float
+     */
+    public function getProductDefaultQty($product = null)
+    {
+        if (!$product) {
+            $product = $this->getProduct();
+        }
+
+        $qty = $this->getMinimalQty($product);
+        $config = $product->getPreconfiguredValues();
+        $configQty = $config->getQty();
+        if ($configQty > $qty) {
+            $qty = $configQty;
+        }
+
+        return $qty;
+    }
+
+    /**
+     * Return identifiers for produced content
+     *
+     * @return array
+     */
+    public function getIdentities()
+    {
+        $identities = $this->getProduct()->getIdentities();
+        $category = $this->_coreRegistry->registry('current_category');
+        if ($category) {
+            $identities[] = Category::CACHE_TAG . '_' . $category->getId();
+        }
+        return $identities;
+    }
+
+    /**
+     * TODO create public function which return price message for add example product button
+     * @param null|\Magento\Catalog\Model\Product $product
+     */
+    public function getPriceMessage($product = null)
+    {
+        $message = 'with Regular Price';
+        if (!$product) {
+            $product = $this->getProduct();
+        }
+        return $message;
+    }
+}
